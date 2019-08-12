3b7e1a734700def25e6af5ac57cf2f6450e5c5f4
Paola Vargas <vrgspaola68@gmail.com>
2019-08-12 14:28:22 -0400

    Add blog layout and blog-single layout

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -10,17 +10,3 @@ $('.multiple-items').slick({
   slidesToScroll: 4,
   arrows: true
 });
-
-$(window).scroll(function () {
-  scroll = $(window).scrollTop()
-  scrollplus = scroll + 400
-  svg_top = $(".svgs.parallax").position().top
-  team_top = $(".faq").position().top
-  team_bottom = $(".faq").outerHeight(true)
-
-  if (((scrollplus) >= team_top) && (scrollplus <= (team_top + team_bottom) - 500)) {
-    $(".svgs.parallax").css({
-      transform: `translateY(${(scrollplus - team_top) * 0.8}px)`,
-    })
-  }
-})
